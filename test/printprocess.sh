32008a58b176dd9d25d8c1664f6ecc8689644193
TK <kauati.toiti@gmail.com>
2022-11-22 11:51:58 +0000

    Erika : PANZEER VOOOR(6)

--- test/printprocess.sh
+++ test/printprocess.sh
@@ -1,58 +1,41 @@
 #!/bin/bash
 
 printprocess(){
-
-
-pids=$1
-sec=$2
-total=$3
-
-echo -e seconds : $sec
-
-# RateR and RateW calculation using pids array
-declare -a rateR
-declare -a rateW
-
-for ((i=0; i<${#pids[@]}; i++)); do
-    
-    cont=${pids[i]}
-    #checkexistence="/proc/${cont}/"
-    readb=$(sudo cat /proc/${cont}/io | grep "rchar" | awk '{print $2}' )
-    writeb=$(sudo cat /proc/${cont}/io | grep "wchar" | awk '{print $2}' )
-    rateR+=($readb)
-    rateW+=($writeb)
-    #echo "Initial rater n ratew"
+declare -a pids
+for p in "$@"; do 
+if [[ $p == "sep" ]]; then
+        break
+fi
+pids+=("$p")        
+#echo $p 
 done
+#pids=("$1")
+#echo total of pids : "${#pids[@]}"
+rateR=$2
+#echo rateR : "${rateR[@]}"
+rateW=$3
+#echo rateW : "${rateW[@]}"
+#total=$2
+#echo "total : $2"
 
-sleep $sec
+
+
 printed=0
 # 3 . 1
 #iteration to read each pid in the file
 for p in "${pids[@]}" ; do
-        if [[ $printed -eq $total ]]; then
-            break
-        fi    
-        echo -e "\n\n\n|-------------------------------(iteration : start)----------------------------------\n"
+        echo -e "\n\n\n|-------------------------------(iteration : $printed start)----------------------------------\n"
         pid=$p
         #just to print the info of the process to compare
         
         echo -e "\nsudo cat /proc/$pid/io :\n-------------------------\n$(sudo cat /proc/$pid/io)\n-------------------------\n"
-        printf "\n%10s %10s %10s %10s %10s %10s %10s %10s %10s %20s" "COMM" "PID" "USER" "READB" "WRITEB" "RATER" "RATEW" "DATE"
+        printf "\n%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s" "COMM" "PID" "USER" "READB" "WRITEB" "RATER" "RATEW" "DATE"
 
         #The command (COMM) that casted the process
         CMD=$(ps -p $pid | awk '{ if ( $4 != "CMD") print $4 ;}' )
         
         #User that cast the command/process
         USER=$(ps -p $pid -F | awk '{ if ( $1 != "UID" ) print $1; }')
-
-        # The value here will obviously have to be changed to a variable derived from the arguments
-        # By omission, we'll use 5 seconds (though I think it's supposed to be 10 but I don't like waiting for too long)
-        readb=$(sudo cat /proc/${pid}/io | grep "rchar" | awk '{print $2}' )
-        writeb=$(sudo cat /proc/${pid}/io | grep "wchar" | awk '{print $2}' )
-        #The rate of chars that were read on this process
-        rateR[$pid]="$(($readb/$sec)).$(( ($readb*100/$sec)%100 ))"
-        #The rate of chars that wre write on this process
-        rateW[$pid]="$(($writeb/$sec)).$(( ($writeb*100/$sec)%100 ))"
 
         #'READB' column
         READB=$(sudo cat /proc/$pid/io | awk '{ if ( $1 == "read_bytes:" ) print $2;}' )
@@ -63,12 +46,12 @@ for p in "${pids[@]}" ; do
         #the datemonth=$(ps -o lstart -p $pid | awk '{ if ( $1 != "STARTED") print $2; }' )
         day=$(ps -o lstart -p $pid | awk '{ if ( $1 != "STARTED") print $3; }' )
         hour=$(ps -o lstart -p $pid | awk '{ if ( $1 != "STARTED") print $4; }' )
-        DATE=$(echo ${month} " " ${day} " " ${hour})
+        DATE=$(echo ${day} " " ${hour})
 
         #Print the process info 
-        printf "\n%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s" $CMD $pid $USER $READB $WRITEB "${rateR[$pid]}" "${rateW[$pid]}" $DATE
+        printf "\n%10s %10s %10s %10s %10s %10s %10s %10s %10s %16s" $CMD $pid $USER $READB $WRITEB "${rateR[$pid]}" "${rateW[$pid]}" $DATE
 
-        echo -e "\n-------------------------------(iteration : end)----------------------------------|\n"
+        echo -e "\n-------------------------------(iteration : $printed end)----------------------------------|\n"
         printed=$(($printed+1))
 done
 
--- test/rwstat.sh
+++ test/rwstat.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Fun fact : using "<< com" and then "com" on separate lines will make it so everything in between is commented
 source "./readpidsbyuser.sh" 
-source "./printprocess.sh"
 
 
 ### Data zone , to store variables that can be changed with the opts or let by deafult
@@ -25,7 +24,7 @@ do
     #The number(seconds) to sleep to calculate the rateR and rateW[
     #standardizing the seconds argument position as the first passed
     if [[ $# -eq $seconds_index ]]; then
-     #echo $1
+     echo $1
      sec=$1
     fi
 
@@ -120,37 +119,125 @@ for ((i=0; i<${#rawpids}; i++)); do
 done
 
 
+
+
+# RateR and RateW calculation using pids array
+declare -a rateR
+declare -a rateW
+
+for ((i=0; i<${#pids[@]}; i++)); do
+    
+    cont=${pids[i]}
+    #checkexistence="/proc/${cont}/"
+    readb=$(sudo cat /proc/${cont}/io | grep "rchar" | awk '{print $2}' )
+    writeb=$(sudo cat /proc/${cont}/io | grep "wchar" | awk '{print $2}' )
+    rateR+=($readb)
+    rateW+=($writeb)
+    #echo "Initial rater n ratew"
+done
+
+sleep $sec
+
+for ((i=0; i<${#pids[@]}; i++)); do
+    pid=${pids[i]}
+        # The value here will obviously have to be changed to a variable derived from the arguments
+        # By omission, we'll use 5 seconds (though I think it's supposed to be 10 but I don't like waiting for too long)
+        readb=$(sudo cat /proc/${pid}/io | grep "rchar" | awk '{print $2}' )
+        writeb=$(sudo cat /proc/${pid}/io | grep "wchar" | awk '{print $2}' )
+        #The rate of chars that were read on this process
+        rateR[$pid]="$(($readb/$sec)).$(( ($readb*100/$sec)%100 ))"
+        #The rate of chars that wre write on this process
+        rateW[$pid]="$(($writeb/$sec)).$(( ($writeb*100/$sec)%100 ))"
+        #echo rateR of pid $pid : ${rateR[$pid]}
+        #echo rateW of pid $pid : ${rateW[$pid]}
+
+done
+
 #echo total : $total
 
-if test -z "$total"
-then
-    #echo total : 0
-    total="${#pids[@]}"
-    echo total : $total
-else
-    echo total : $total
-fi       
+   
 
+#echo total : $total
+#echo final pids : "${pids[@]}"
 
 if [[ $reverse -eq 1 ]]; then
 
+    #Reverse the rateR
     Min=0
-    Max=$(( ${#pids[@]} -1 ))
+    Max=$(( ${#rateR[@]} -1 ))
 
     while [[ Min -lt Max ]]
     do
         # Swap current first and last elements
-        x="${pids[$Min]}"
-        pids[$Min]="${pids[$Max]}"
-        pids[$Max]="$x"
+        x="${rateR[$Min]}"
+        rateR[$Min]="${rateR[$Max]}"
+        rateR[$Max]="$x"
 
         # Move closer
         (( Min++, Max-- ))
     done
 
-    echo reversed order pids : "${pids[@]}"
-fi
-#echo seconds first : $sec
+    echo reversed order rateR : "${rateR[@]}"
 
-#Print the process in table format
-printprocess $pids $sec $total
+
+    #Reverse the rateW
+    Min=0
+    Max=$(( ${#rateW[@]} -1 ))
+
+    while [[ Min -lt Max ]]
+    do
+        # Swap current first and last elements
+        x="${rateW[$Min]}"
+        rateW[$Min]="${rateW[$Max]}"
+        rateW[$Max]="$x"
+
+        # Move closer
+        (( Min++, Max-- ))
+    done
+
+    echo reversed order rateW : "${rateW[@]}"
+fi
+
+
+echo "seconds first : $sec"
+
+source "./printprocess.sh"
+
+if test -z "$total"
+then
+    #echo total : 0
+    #total="${#pids[@]}"
+    #echo total : $total
+     #Print the process in table format
+    #            $1            $2            $3               
+    printprocess "${pids[@]}"  "${rateR[@]}" "${rateW[@]}"  
+
+else
+    echo total defined : $total
+    declare -a finalPids
+    n=0
+    for ((i=0;i<${#pids[@]};i++)){
+        if [[ $n -eq $total ]]; then
+        break
+        fi
+        pid=${pids[i]}
+        finalPids+=($pid)
+        echo pid : $n
+        echo final pids : ${finalPids[@]}
+
+           n=$((n+1)) 
+    }
+    echo end on pids : ${finalPids[@]}
+    #            $1                $2      $3            $4              
+    printprocess "${finalPids[@]}" "sep"   "${rateR[@]}" "${rateW[@]}"
+    #source "./testargs.sh"
+
+    #func ${finalPids[@]}
+
+
+    #pids=${finalPids[@]}
+    #set -v -x
+    #Print the process in table format
+
+fi    
+
